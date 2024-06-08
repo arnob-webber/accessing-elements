0d3759e35f7db66c31cae42d1efb3a6c58f7fcef
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-06-08 22:57:03 +0600

    uu

--- code.js
+++ code.js
@@ -4,5 +4,9 @@ let byId = document.getElementById("yourId");
 // by class
 let byClass = document.getElementsByClassName("yourClassName");
 
-//by tag 
-let byTag = document.getElementsByTagName('div, body')
+//by tag
+let byTag = document.getElementsByTagName("div, body");
+
+//by css
+
+let byCss = document.querySelectorAll(".yourClassName");
