176069d9d5241920c6f17698de5ea3e80b99797b
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-06-08 22:54:53 +0600

    dom js

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
-// By ID
+// by id
+let byId = document.getElementById("yourId");
 
-let byId = document.getElementById('yourId')
+// by class
+let byClass = document.getElementsByClassName("yourClassName");
 
+//by tag 
+let byTag = document.getElementsByTagName('div, body')
